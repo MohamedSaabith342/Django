0e40f2fae516591232a61e379b597c3a0b60a6b0
MohamedSaabith342 <e21342@eng.pdn.ac.lk>
2025-06-27 22:38:24 +0530

    style.css file added

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello world,You are at blog index")
+  return render(request,'blog/index.html')
 
 def detail(request,post_id):
-  return HttpResponse(f"You are viewing post detail page. And ID is {post_id}")
+  return render(request,'blog/detail.html')
 
 
 def old_url_redirect(request):
